c5750b3e54c892e901ff71550c5cc5325d61258a
Duy Nguyen <dcnguyen060899@gmail.com>
2026-01-01 00:18:39 -0800

    Fix gradio_client API schema bug by disabling show_api

--- ml-demos/garbage-classification/app.py
+++ ml-demos/garbage-classification/app.py
@@ -84,4 +84,4 @@ demo = gr.Interface(
 )
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch(server_name="0.0.0.0", server_port=7860, show_api=False)
